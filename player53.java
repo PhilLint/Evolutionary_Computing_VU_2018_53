8786cc4a5f8f2b8be1890893941f9f816fd88cad
Dirk Hoekstra <dirkhoekstra@gmail.com>
2018-09-10 18:38:49 +0200

    Rename variables, yay

--- player53.java
+++ player53.java
@@ -6,60 +6,75 @@ import java.util.Properties;
 
 public class player53 implements ContestSubmission
 {
-	Random rnd_;
-	ContestEvaluation evaluation_;
-    private int evaluations_limit_;
-	
+	private Random random;
+
+	private ContestEvaluation evaluation;
+
+	private int evaluationsLimit;
+
 	public player53()
 	{
-		rnd_ = new Random();
+		random = new Random();
 	}
-	
+
 	public void setSeed(long seed)
 	{
 		// Set seed of algortihms random process
-		rnd_.setSeed(seed);
+		random.setSeed(seed);
 	}
 
 	public void setEvaluation(ContestEvaluation evaluation)
 	{
 		// Set evaluation problem used in the run
-		evaluation_ = evaluation;
-		
+		this.evaluation = evaluation;
+
 		// Get evaluation properties
 		Properties props = evaluation.getProperties();
-        // Get evaluation limit
-        evaluations_limit_ = Integer.parseInt(props.getProperty("Evaluations"));
+		// Get evaluation limit
+		evaluationsLimit = Integer.parseInt(props.getProperty("Evaluations"));
 		// Property keys depend on specific evaluation
 		// E.g. double param = Double.parseDouble(props.getProperty("property_name"));
-        boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
-        boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
-        boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
+		boolean isMultimodal = Boolean.parseBoolean(props.getProperty("Multimodal"));
+		boolean hasStructure = Boolean.parseBoolean(props.getProperty("Regular"));
+		boolean isSeparable = Boolean.parseBoolean(props.getProperty("Separable"));
 
 		// Do sth with property values, e.g. specify relevant settings of your algorithm
-        if(isMultimodal){
-            // Do sth
-        }else{
-            // Do sth else
-        }
-    }
-    
+		if(isMultimodal){
+		    // Do sth
+		} else {
+		    // Do sth else
+		}
+	}
+
 	public void run()
 	{
 		// Run your algorithm here
-        
-        int evals = 0;
-        // init population
-        // calculate fitness
-        while(evals<evaluations_limit_){
-            // Select parents
-            // Apply crossover / mutation operators
-            double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
-            // Check fitness of unknown fuction
-            Double fitness = (double) evaluation_.evaluate(child);
-            evals++;
-            // Select survivors
-        }
+		int evals = 0;
 
+		// init population
+
+
+		// calculate fitness
+
+
+		while(evals<evaluationsLimit){
+		    // Select parents
+
+
+		    // Apply crossover / mutation operators
+
+
+		    double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+		    // Check fitness of unknown fuction
+
+
+		    Double fitness = (double) evaluation.evaluate(child);
+		    evals++;
+
+
+		    // Select survivors
+
+
+		}
 	}
 }
